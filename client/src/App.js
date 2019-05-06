ba096e1229e28dbc702b209f1933bc15792ac40d
b06to587 <b06to587@gmail.com>
2019-05-06 15:42:41 +0900

    Create Customer Database Table & NodeJs Database Connection setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Paper from '@material-ui/core/Paper'
 import Customer from './components/Customer';
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req,res)=>{
-    res.send([
-            {
-            'id' : 1,
-            'image' : 'https://placeimg.com/64/64/1',
-            'name' : '홍길동',
-            'birthday' : '910222',
-            'gender':'여자',
-            'job': '대학생'
-          },
-          {
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/2',
-            'name' : '토르',
-            'birthday' : '930510',
-            'gender':'남자',
-            'job': '신'
-          },
-          {
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/any',
-            'name' : '아이언맨',
-            'birthday' : '840312',
-            'gender':'남자',
-            'job': '프로그래머'
-          }
-    ]);
+    connection.query(
+       "SELECT * FROM CUSTOMER",
+       (err, rows, fields) => {
+           res.send(rows);
+       }
+   );
 });
 
 app.listen(port,()=> console.log(`Listenging on port ${port}`));
